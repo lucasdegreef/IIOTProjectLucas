c97b7aa21b183e21ec4442a0e04eb5bb06f361bb
lucasdegreef <lucasdegreef@live.com>
2026-03-30 23:01:09 +0200

    Add C# OpcuaConnector component and update .gitignore for Visual Studio

---
+++ CsharpOpcuaConnector/OpcuaConnectorLucas/OpcuaConnectorLucas/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OpcuaConnectorLucas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world"); 
+
+            var getal = Console.ReadLine();
+
+            Console.WriteLine("you gave me: " + getal);
+
+
+        }
+    }
+}
